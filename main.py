ac36dfcdecb049ecd387c2d3aa59e7c4e319537d
Alex Frankiv <alex.frankiv13@gmail.com>
2020-11-22 17:42:13 +0200

    Basic UI drawing done; experiencing trouble with threading

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from checkers.game import Game
+from board_drawing import BDManager
 
+## Init components
 game = Game()
-print(game.whose_turn())
+uiManager = BDManager(game=game)
+
+## Main
+# TODO: start server and move this to server
+# WARN: This is for test purposes only
+# sleep(5)
+game.move([9, 13])
+uiManager.setNeedsUpdate()
+print("move done")
